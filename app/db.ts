52aed045d90313715e4c15b68ffb3a7aef78657f
moonymax <115226957+moonymax@users.noreply.github.com>
2024-05-22 23:17:40 +0200

    add drizzle

--- app/db.ts
+++ app/db.ts
@@ -1,12 +1,22 @@
+import { drizzle } from "drizzle-orm/libsql";
 import { createClient } from "@libsql/client";
 
+import { sql } from "drizzle-orm";
+import { text, integer, sqliteTable } from "drizzle-orm/sqlite-core";
+
 const client = createClient({
-  url: "file:replica.db",
-  syncUrl: "libsql://...",
-  authToken: "...",
+  url: "file:./local.db",
+  //authToken: "DATABASE_AUTH_TOKEN",
 });
 
-const result = await client.execute({
-  sql: "SELECT * FROM users WHERE id = ?",
-  args: [1],
+export const db = drizzle(client);
+
+export const users = sqliteTable("users", {
+  id: text("id"),
+  textModifiers: text("text_modifiers")
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  intModifiers: integer("int_modifiers", { mode: "boolean" })
+    .notNull()
+    .default(false),
 });
